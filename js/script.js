b2f42a118e1cc7dbe0a79aa688135fb4a9ced414
peter Huang <100703042@nccu.edu.tw>
2014-06-08 20:22:22 +0800

    v2

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ FB.getLoginStatus(function(response) {
   if (response.status === 'connected') {
     //呼叫api把圖片放到#preview IMG tag 內
     	FB.api('/me/picture?type=normal', function(response) {
-			var str="<img src="+ response.data.url +">";
+			var str="<img id='preview1' crossOrigin='Anonymous' src="+ response.data.url +">";
 			$('body').append(str);
 		});
 
@@ -99,7 +99,7 @@ FB.getLoginStatus(function(response) {
       if(isDragging){ //當拖拉為True時
           	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 			var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
-			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
+			//profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			//canvas.height = profileIMG.height;
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
