fc6ea0f0db84fb39dde24e13509cb09cda6773ee
peter Huang <100703042@nccu.edu.tw>
2014-06-08 22:08:44 +0800

    a

--- js/script.js
+++ js/script.js
@@ -103,9 +103,11 @@ FB.getLoginStatus(function(response) {
 			//profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			//canvas.height = profileIMG.height;
+            ctx.drawImage(img2,0,0); //劃入img2
+
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
 			ctx.drawImage(img3,canMouseX-128/2,canMouseY-120/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
-			ctx.drawImage(img2,0,0); //劃入img2
+			
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 			ctx.fillStyle = "black"; //字體顏色
 			ctx.font='20px "微軟正黑體"'; //字體大小和字形
